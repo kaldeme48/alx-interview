be874ef274dde75d0b2f0d01b50ddd005aefb841
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2023-02-23 12:21:14 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,16 @@
 def makeChange(coins, total):
     """Finds the minimum number of coins needed to make change
     """
+    sum = 0
     if (total <= 0):
         return 0
-    coin_arr = [float('inf') for i in range(total + 1)]
-    coin_arr[0] = 0
-    for coin in coins:
-        for x in range(len(coin_arr)):
-            if coin <= x:
-                coin_arr[x] = min(coin_arr[x], 1 + coin_arr[x - coin])
-    return coin_arr[total] if coin_arr[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
